a2026f270c2f9316c63b12f754880bf6093d6f14
milena-mognon <milenamognon@gmail.com>
2023-11-19 15:12:02 -0300

    feat: slug value object and test

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,20 +1,24 @@
 import { randomUUID } from "crypto";
+import { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
   title: string;
   content: string;
+  slug: Slug;
   authorId: string;
 }
 
 export class Question {
   id: string;
   title: string;
+  slug: Slug;
   content: string;
   authorId: string;
 
   constructor(props: QuestionProps, id?: string) {
     this.title = props.title;
     this.content = props.content;
+    this.slug = props.slug;
     this.authorId = props.authorId;
     this.id = id ?? randomUUID();
   }
---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,28 @@
+export class Slug {
+  public value: string;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  /**
+   * Receives a string and normalize ir as a slug
+   *
+   * Example: "An Exemple Title" => "an-example-title"
+   *
+   * @param text {string}
+   */
+  static createFromText(text: string) {
+    const slugText = text
+      .normalize("NFKD") // remove todos os caracteres com acentos e transforma em sem acento
+      .toLowerCase() // converte para letra minuscula
+      .trim() // remove espaços no final
+      .replace(/\s+/g, "-") // substitui todos os espaços em branco hífen
+      .replace(/[^\w-]+/g, "") // substitui tudo o que não são palavras por string vazia
+      .replace(/_/g, "-") // substitui todos os underlines _ por hífen -
+      .replace(/--+/g, "-") // substitui dois hífen juntos -- por um só -
+      .replace(/-$/g, ""); // remove hífen no final
+
+    return new Slug(slugText);
+  }
+}
